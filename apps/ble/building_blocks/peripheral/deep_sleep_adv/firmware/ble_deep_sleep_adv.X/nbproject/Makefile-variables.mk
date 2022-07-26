54acf6e075337cef65e6554ce5982ad91f928680
Saravanakumar Sundaram <Saravanakumar.Sundaram@Microchip.com>
2022-07-26 12:50:43 +0530

    [APPS] WBZ451 Application RTP Release - Remove Private folder & Added Deep Sleep Application WSCH-185

---
+++ apps/ble/building_blocks/peripheral/deep_sleep_adv/firmware/ble_deep_sleep_adv.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ble_deep_sleep_adv.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ble_deep_sleep_adv.X.production.hex
---
+++ apps/ble/building_blocks/peripheral/deep_sleep_adv/firmware/src/app_ble/app_trsps_handler.h
@@ -0,0 +1,80 @@
+/*******************************************************************************
+  MPLAB Harmony Application Header File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app_trsps_handler.h
+
+  Summary:
+    This header file provides prototypes and definitions for the application.
+
+  Description:
+    This header file provides function prototypes and data type definitions for
+    the application.  Some of these are required by the system (such as the
+    "APP_Initialize" and "APP_Tasks" prototypes) and some of them are only used
+    internally by the application (such as the "APP_STATES" definition).  Both
+    are defined here for convenience.
+*******************************************************************************/
+
+#ifndef _APP_TRSPS_HANDLER_H
+#define _APP_TRSPS_HANDLER_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stdint.h>
+#include <string.h>
+#include "ble_trsps/ble_trsps.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Type Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_TrspsEvtHandler(BLE_TRSPS_Event_T *p_event)
+
+  Summary:
+     BLE application Transparent profile server event handler.
+
+  Description:
+
+  Precondition:
+
+  Parameters:
+    None.
+
+  Returns:
+    None.
+
+*/
+void APP_TrspsEvtHandler(BLE_TRSPS_Event_T *p_event);
+
+
+#endif /* _APP_TRSPS_HANDLER_H */
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+/*******************************************************************************
+ End of File
+ */
+
