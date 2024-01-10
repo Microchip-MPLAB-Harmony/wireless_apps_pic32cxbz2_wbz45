196d6208bbaea6a0e04c6f26b6af9d93fef9a093
Manu Harshan <manu.harshan@microchip.com>
2024-01-10 12:13:59 +0530

    [WSCH-376] updated the docs folder

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240105154547';
+                args = '20240109130151';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
---
+++ docs/oxygen-webhelp/app/nav-links/json/GUID-A8FBF1F1-80DD-42AA-82E8-3935CBABFB82-d1760e1667.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"<ph>13.5.1.1<\/ph>\nAPP_TIMER_TIMEOUT","href":"GUID-A7AE34A9-87E8-4AC1-8D72-8002DBC06789.html","attributes":{"data-id":"app-timer-timeout"},"menu":{"hasChildren":false},"tocID":"app-timer-timeout-d1760e1675","topics":[]}]});
