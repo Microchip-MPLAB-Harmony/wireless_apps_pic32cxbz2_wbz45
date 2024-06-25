25ceef035a4fdf86a0530900cedb21b37aa80789
Raja Shruthi <Raja.Shruthi@microchip.com>
2024-06-25 12:29:54 +0530

    [WSCH-440] Code clean up

--- apps/multiprotocol/ble_mac_prov_ffd/firmware/ble_mac_prov_ffd.X/build/default/production/_ext/791560404/hs2xy.o.d
+++
@@ -1,2 +0,0 @@
-build/default/production/_ext/791560404/hs2xy.o: \
- ../src/sensors/src/hs2xy.c
--- apps/multiprotocol/ble_mac_prov_ffd/firmware/ble_mac_prov_ffd.X/build/default/production/_ext/791560404/temp_sensor.o.d
+++
@@ -1,2 +0,0 @@
-build/default/production/_ext/791560404/temp_sensor.o: \
- ../src/sensors/src/temp_sensor.c
