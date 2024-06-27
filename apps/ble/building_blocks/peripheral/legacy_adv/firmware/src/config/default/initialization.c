a06683ebec5344d3bc1da81c0a12733ffd2d43a7
Dinesh Arasu <dinesh.arasu@microchip.com>
2024-06-27 14:37:09 +0530

    [WSCH-426] updated the PXPM and legacy_adv ble applications

--- apps/ble/building_blocks/peripheral/legacy_adv/firmware/src/config/default/initialization.c
+++ apps/ble/building_blocks/peripheral/legacy_adv/firmware/src/config/default/initialization.c
@@ -406,9 +406,9 @@ void SYS_Initialize ( void* data )
 
 	GPIO_Initialize();
 
-    EVSYS_Initialize();
-
     SERCOM0_USART_Initialize();
+
+    EVSYS_Initialize();
 
     RTC_Initialize();
 
@@ -471,7 +471,6 @@ void SYS_Initialize ( void* data )
 
     osalAPIList.OSAL_MemAlloc = OSAL_Malloc;
     osalAPIList.OSAL_MemFree = OSAL_Free;
-
 
 
 
